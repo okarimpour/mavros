204cc1d9b71b536e53496fe7348f4a391382857a
Omid Karimpour <okarimpour@gmail.com>
2020-04-28 21:25:19 -0400

    Added a off boarding node and changed the CMakeLists accordingly

--- mavros/src/offb_node.cpp
+++ mavros/src/offb_node.cpp
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
             ("mavros/set_mode");
 
     //the setpoint publishing rate MUST be faster than 2Hz
-    ros::Rate rate(20.0);
+    ros::Rate rate(10.0);
 
     // wait for FCU connection
     while(ros::ok() && !current_state.connected){
